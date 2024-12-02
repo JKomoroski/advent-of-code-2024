77f6b6760e72608172b4f177102a0f6b81681475
Jared Komoroski <jkomoroski@alianza.com>
2024-12-02 11:19:28 -0700

    Prepare Day 3

---
+++ src/main/java/ski/komoro/aoc/twentyfour/Day03.java
@@ -0,0 +1,24 @@
+package ski.komoro.aoc.twentyfour;
+
+import java.util.stream.Stream;
+
+public class Day03 extends AOCBase {
+
+    public static void main(final String[] args) throws Exception {
+        new Day03().run();
+    }
+
+    Day03() {
+        super("day-3", "test.txt");
+    }
+
+    @Override
+    void part1(final Stream<String> fileInput) throws Exception {
+
+    }
+
+    @Override
+    void part2(final Stream<String> fileInput) throws Exception {
+
+    }
+}
