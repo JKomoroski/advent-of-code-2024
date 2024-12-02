3ff5ec567b44db811d3004cbe40aa51bfc4ba18a
Jared Komoroski <jkomoroski@alianza.com>
2024-12-02 10:58:44 -0700

    Day 2

--- src/main/java/Utils.java
+++ src/main/java/Utils.java
@@ -1,4 +1,27 @@
-package PACKAGE_NAME;
+import java.util.Collection;
+import java.util.Comparator;
 
 public class Utils {
+    private Utils() {
+        throw new IllegalStateException("Utility class");
+    }
+
+    public static <T> boolean isSorted(Collection<T> collection, Comparator<T> comparator) {
+        if (collection.isEmpty() || collection.size() == 1) {
+            return true;
+        }
+
+        var iter = collection.iterator();
+        T current, previous = iter.next();
+
+        while (iter.hasNext()) {
+            current = iter.next();
+            if (comparator.compare(previous, current) > 0) {
+                return false;
+            }
+            previous = current;
+        }
+
+        return true;
+    }
 }
