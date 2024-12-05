4f9cd2bf951ad9a776f3b908edfc45c489b914fb
Jared Komoroski <jkomoroski@alianza.com>
2024-12-05 08:26:19 -0700

    Prepare Day 6

--- src/main/java/ski/komoro/aoc/twentyfour/Day05.java
+++ src/main/java/ski/komoro/aoc/twentyfour/Day05.java
@@ -2,13 +2,11 @@ package ski.komoro.aoc.twentyfour;
 
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Collections;
 import java.util.Comparator;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
-import java.util.Objects;
 import java.util.Set;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
---
+++ src/main/java/ski/komoro/aoc/twentyfour/Day06.java
@@ -0,0 +1,25 @@
+package ski.komoro.aoc.twentyfour;
+
+import java.util.stream.Stream;
+
+public class Day06 extends AOCBase {
+
+
+    public static void main(final String[] args) throws Exception {
+        new Day06().run();
+    }
+
+    private Day06() {
+        super("day-6", "test.txt");
+    }
+
+    @Override
+    protected void part1(final Stream<String> fileInput) throws Exception {
+
+    }
+
+    @Override
+    protected void part2(final Stream<String> fileInput) throws Exception {
+
+    }
+}
