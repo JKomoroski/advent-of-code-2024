aeafd0029f5fddcf869baa2412c99ded35365b41
Jared Komoroski <jkomoroski@alianza.com>
2024-12-05 10:01:02 -0700

    Refactor Day 5

--- src/main/java/ski/komoro/aoc/twentyfour/Day05.java
+++ src/main/java/ski/komoro/aoc/twentyfour/Day05.java
@@ -3,13 +3,14 @@ package ski.komoro.aoc.twentyfour;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Comparator;
-import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
-import java.util.Map;
 import java.util.Set;
+import java.util.function.Predicate;
 import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 import java.util.stream.Stream;
+import ski.komoro.aoc.utils.Tuple;
 
 public class Day05 extends AOCBase {
 
@@ -24,46 +25,13 @@ public class Day05 extends AOCBase {
 
     @Override
     protected void part1(final Stream<String> fileInput) throws Exception {
-        Map<Integer, Set<Integer>> rules = new HashMap<>();
+        Set<Tuple<Integer, Integer>> rules = new HashSet<>();
         List<List<Integer>> updates = new ArrayList<>();
-        fileInput.iterator().forEachRemaining(line -> {
-            if (line.contains("|")) {
-                final var split = line.split("\\|");
-                final var k = Integer.parseInt(split[0]);
-                final var r = Integer.parseInt(split[1]);
-                final var v = rules.get(k);
-                if (v == null) {
-                    final var s = new HashSet<Integer>();
-                    s.add(r);
-                    rules.put(k, s);
-                } else {
-                    v.add(r);
-                }
-            }
-            if (line.contains(",")) {
-                final var split = Arrays.stream(line.split(","))
-                        .map(Integer::parseInt)
-                        .toList();
-                updates.add(split);
-            }
-        });
+        parse(fileInput, rules, updates);
 
+        final var correctFilter = isCorrect(rules);
         long sumOfCorrectMiddles = updates.stream()
-                .filter(u -> {
-                    for (var i = 0; i < u.size(); i++) {
-                        final var rule = u.get(i);
-                        final var after = rules.get(rule);
-                        if (after == null) {
-                            continue;
-                        }
-                        for (var j = i; j >= 0; j--) {
-                            if (after.contains(u.get(j))) {
-                                return false;
-                            }
-                        }
-                    }
-                    return true;
-                })
+                .filter(correctFilter)
                 .mapToLong(u -> u.get(u.size() / 2))
                 .sum();
 
@@ -72,47 +40,15 @@ public class Day05 extends AOCBase {
 
     @Override
     protected void part2(final Stream<String> fileInput) throws Exception {
-        Map<Integer, Set<Integer>> rules = new HashMap<>();
+        Set<Tuple<Integer, Integer>> rules = new HashSet<>();
         List<List<Integer>> updates = new ArrayList<>();
-        fileInput.iterator().forEachRemaining(line -> {
-            if (line.contains("|")) {
-                final var split = line.split("\\|");
-                final var k = Integer.parseInt(split[0]);
-                final var r = Integer.parseInt(split[1]);
-                final var v = rules.get(k);
-                if (v == null) {
-                    final var s = new HashSet<Integer>();
-                    s.add(r);
-                    rules.put(k, s);
-                } else {
-                    v.add(r);
-                }
-            }
-            if (line.contains(",")) {
-                final var split = Arrays.stream(line.split(","))
-                        .map(Integer::parseInt)
-                        .collect(Collectors.toList());
-                updates.add(split);
-            }
-        });
+        parse(fileInput, rules, updates);
 
-        final var ruleComparator = new RuleComparator(rules);
+        final var incorrectFilter = Predicate.not(isCorrect(rules));
+        final Comparator<Integer> ruleComparator = (o1, o2) -> rules.contains(Tuple.of(o1, o2)) ? -1 : 1;
+
         long sumOfFixedMiddles = updates.stream()
-                .filter(u -> {
-                    for (var i = 0; i < u.size(); i++) {
-                        final var rule = u.get(i);
-                        final var after = rules.get(rule);
-                        if (after == null) {
-                            continue;
-                        }
-                        for (var j = i; j >= 0; j--) {
-                            if (after.contains(u.get(j))) {
-                                return true;
-                            }
-                        }
-                    }
-                    return false;
-                })
+                .filter(incorrectFilter)
                 .map(u -> {
                     u.sort(ruleComparator);
                     return u;
@@ -123,24 +59,26 @@ public class Day05 extends AOCBase {
         System.out.println("Part 2: " + sumOfFixedMiddles);
     }
 
-    class RuleComparator implements Comparator<Integer> {
-
-        private final Map<Integer, Set<Integer>> rules;
-
-        RuleComparator(final Map<Integer, Set<Integer>> rules) {
-            this.rules = rules;
-        }
-
-        @Override
-        public int compare(final Integer o1, final Integer o2) {
-            final var v = rules.get(o1);
-            if (v == null) {
-                return 0;
+    private void parse(Stream<String> fileInput, final Set<Tuple<Integer, Integer>> rules, final List<List<Integer>> updates) {
+        fileInput.forEach(line -> {
+            if (line.contains("|")) {
+                final var split = line.split("\\|");
+                rules.add(Tuple.of(Integer.parseInt(split[0]), Integer.parseInt(split[1])));
             }
-            if (v.contains(o2)) {
-                return -1;
+
+            if (line.contains(",")) {
+                final var split = Arrays.stream(line.split(","))
+                        .map(Integer::parseInt)
+                        .collect(Collectors.toList());
+                updates.add(split);
             }
-            return 1;
-        }
+        });
+    }
+
+    private Predicate<List<Integer>> isCorrect(final Set<Tuple<Integer, Integer>> rules) {
+        return u -> IntStream.range(0, u.size())
+                .boxed()
+                .flatMap(i -> IntStream.rangeClosed(0, i).mapToObj(j -> Tuple.of(u.get(i), u.get(j))))
+                .noneMatch(rules::contains);
     }
 }
